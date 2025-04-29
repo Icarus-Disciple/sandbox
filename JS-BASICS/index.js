db43ef69eb4ba66ceb294b0d23ee1fa5884cf588
Icarus-Disciple <icarus.disciple@gmail.com>
2025-04-29 21:28:17 +0000

    adding first iteration of files to see a commit

---
+++ JS-BASICS/index.js
@@ -0,0 +1,12 @@
+// comment
+
+let selectedColors = ['Red','Blue'];
+selectedColors[2] = 'Green';
+console.log(selectedColors[2])
+
+function greet(name) {
+    console.log('Hello ' + name);
+
+}
+
+greet('John');
